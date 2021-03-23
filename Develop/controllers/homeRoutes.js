d51bd75ccbb92b9883a56b8dfc31d4ae3cad1672
kate <katy.madden99@gmail.com>
2021-03-23 21:34:19 +0000

    Updated homeroutes to include dashboard

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
-const { Blog } = require('../models');
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     const blogData = await Blog.findAll({
       order: [['name', 'ASC']],
+      include: [
+        {
+            model: User,
+            attributes: ['id', 'name']
+        },
+      ]
     });
+
+    console.log(blogData);
 
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
@@ -36,6 +45,27 @@ router.get('/signup', (req, res) => {
   }
 
   res.render('signup');
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+
+    const userBlogs = await Blog.findAll({ 
+      where: { user_id: 2 },
+      order: [['name', 'ASC']], 
+    });
+
+    const myBlogs = userBlogs.map((blog) => blog.get({ plain: true }));
+
+    res.render('dashboard', {
+      myBlogs,
+      logged_in: req.session.logged_in,
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
